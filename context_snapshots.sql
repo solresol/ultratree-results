2a7c8f421842c07f325d94cc68b45e15b0562322
Greg Baker <greg.baker@anu.edu.au>
2025-01-16 17:30:40 +1100

    Automatic updates 2025-01-16

--- context_snapshots.sql
+++ context_snapshots.sql
@@ -65,4 +65,9 @@ INSERT INTO context_snapshots VALUES(57,'/ultratree/language-model/sense-annotat
 INSERT INTO context_snapshots VALUES(58,'/ultratree/language-model/sense-annotated4.sqlite','2025-01-15 13:12:39');
 INSERT INTO context_snapshots VALUES(59,'/ultratree/language-model/sense-annotated5.sqlite','2025-01-15 13:15:41');
 INSERT INTO context_snapshots VALUES(60,'/ultratree/language-model/unannotated-model1.sqlite','2025-01-15 13:37:33');
+INSERT INTO context_snapshots VALUES(61,'/ultratree/language-model/sense-annotated1.sqlite','2025-01-16 05:40:40');
+INSERT INTO context_snapshots VALUES(62,'/ultratree/language-model/sense-annotated2.sqlite','2025-01-16 05:44:22');
+INSERT INTO context_snapshots VALUES(63,'/ultratree/language-model/sense-annotated3.sqlite','2025-01-16 05:48:03');
+INSERT INTO context_snapshots VALUES(64,'/ultratree/language-model/sense-annotated4.sqlite','2025-01-16 05:51:06');
+INSERT INTO context_snapshots VALUES(65,'/ultratree/language-model/sense-annotated5.sqlite','2025-01-16 05:54:16');
 COMMIT;
